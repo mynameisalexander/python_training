bab1e68f37840a2152833664da0736e3c1c841da
mynameisalexander <mr-zion@rambler.ru>
2016-03-18 03:32:33 +0300

    Задание №5
    добавил помощника по работе с контактами + создать класс-менеджер,
    который инициализирует всех помощников

---
+++ model/manager.py
@@ -0,0 +1,7 @@
+class Manager:
+
+    def __init__(self, app, session, group, contact):
+        self.session = session(app)
+        self.group = group(app)
+        self.contact = contact(app)
+
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,12 +12,12 @@ def app(request):
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.create(Group(name="sdfgsdfg", header="sdfgdsfg", footer="sdfgsdfgs"))
-    app.session.logout()
+    app.helpers.session.login(username="admin", password="secret")
+    app.helpers.group.create(Group(name="sdfgsdfg", header="sdfgdsfg", footer="sdfgsdfgs"))
+    app.helpers.session.logout()
 
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
+    app.helpers.session.login(username="admin", password="secret")
+    app.helpers.group.create(Group(name="", header="", footer=""))
+    app.helpers.session.logout()
